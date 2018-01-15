1b5058f28bd2e4e1b72d036298104b2032a4d36e
Mihail Milkov <Mihail_Milkov@epam.com>
2018-01-15 16:25:29 +0200

    Layout Changes

--- app/src/main/java/com/example/mihail_milkov/coolcalc/CalcActivity.java
+++ app/src/main/java/com/example/mihail_milkov/coolcalc/CalcActivity.java
@@ -9,5 +9,7 @@ public class CalcActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
+
+        System.out.println("Test");
     }
 }
